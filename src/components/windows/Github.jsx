560386c3cce6cdc9466d739a37eb3bb4098d685a
anilsolanki9 <anilsolanki1192@gmail.com>
2026-02-16 12:37:42 +0530

    projects data corrected

--- src/components/windows/Github.jsx
+++ src/components/windows/Github.jsx
@@ -3,51 +3,57 @@ import GithubData from "../../assets/Github.json";
 import MacWindow from "./MacWindow";
 import "./github.scss";
 
-const Github = ({
-  windowName,
-  setWindowState,
-  isFullScreen,
-  setIsFullScreen,
-}) => {
+const Github = ({ windowName, setWindowState, isFullScreen, setIsFullScreen }) => {
   return (
     <MacWindow
       windowName={windowName}
       setWindowState={setWindowState}
       isFullScreen={isFullScreen}
-      setIsFullScreen={setIsFullScreen}
-    >
+      setIsFullScreen={setIsFullScreen}>
       {/* As children pass hoga .cards pura in the MacWindow */}
       <div className="cards">
         {GithubData.map((project, idx) => {
           return (
             <div key={idx} className="card">
-              <img
-                loading="eager"
-                width="100%"
-                height="200px"
-                src={project.image}
-                alt=""
-              />
-              <h2>{project.title}</h2>
-              <p className="description">{project.description}</p>
+              <div className="content">
+                <div className="card-image">
+                  <img
+                    loading="eager"
+                    width="100%"
+                    height="200px"
+                    src={project.image}
+                    alt=""
+                  />
+                </div>
+                <h2>{project.title}</h2>
+                <p className="description">{project.description}</p>
 
-              <div className="tags">
-                {project.tags.map((tag, idx) => {
-                  return tag ? (
-                    <p key={idx} className="tag">
-                      {tag}
-                    </p>
-                  ) : (
-                    "Loading..."
-                  );
-                })}
+                <div className="tags">
+                  {project.tags.map((tag, idx) => {
+                    return tag ? (
+                      <p key={idx} className="tag">
+                        {tag}
+                      </p>
+                    ) : (
+                      "Loading..."
+                    );
+                  })}
+                </div>
               </div>
 
               <div className="urls">
-                <a target="_blank" href={project.repoLink}>
+                <a
+                  title="Go to Github Repository"
+                  className="repoLink"
+                  target="_blank"
+                  href={project.repoLink}>
                   Repsitory
                 </a>
-                <a target="_blank" href={project.demoLink}>
+                <a
+                  title="Open Live Webpage"
+                  className="liveLink"
+                  target="_blank"
+                  href={project.demoLink}>
                   Live Link
                 </a>
               </div>
